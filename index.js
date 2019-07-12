688da2733836c7df21ee911ca646bfb7ac259d86
Jonathan Fabricio Seibel <jonathan.seibel@gmail.com>
2019-07-12 15:43:27 -0300

    bora

--- index.js
+++ index.js
@@ -10,16 +10,16 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 app.use(bodyParser.json({ type: 'application/vnd.api+json' }));
 
-// app.use(function(req, res, next) {
-//   res.setHeader('Access-Control-Allow-Origin', 'http://localhost:4200');
-//   res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
-//   res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With, content-type, authorization');
-//   if (req.method === 'OPTIONS') {
-//     res.status(200).send();
-//   } else {
-//     next();
-//   }
-// });
+app.use(function(req, res, next) {
+  res.setHeader('Access-Control-Allow-Origin', 'http://localhost:4200');
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+  res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With, content-type, authorization');
+  if (req.method === 'OPTIONS') {
+    res.status(200).send();
+  } else {
+    next();
+  }
+});
 
 // const NAME_SPACE = '/v1';
 // app.use(NAME_SPACE, require('./app/controllers/token'));
